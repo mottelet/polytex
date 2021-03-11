da4aefe38a15bd4a37eacb1d5b3f9aed0117b07a
mottelet <stephane.mottelet@utc.fr>
2021-03-11 18:38:07 +0100

    cleaning

--- distributions/autres/PolyTeX/windows/bin/polybib.bat
+++
@@ -1,2 +0,0 @@
-@if exist .aux\%1-poly.aux bibtex .aux\%1-poly
-@if exist %1-poly.aux bibtex %1-poly
--- distributions/autres/PolyTeX/windows/bin/polybooklet.bat
+++
@@ -1,5 +0,0 @@
-psbook %1.ps temp1
-psnup -2 temp1 temp2  
-pstops 2:0@1,1@1U(21cm,29.7cm) temp2 %1.booklet.ps
-@del temp1
-@del temp2
